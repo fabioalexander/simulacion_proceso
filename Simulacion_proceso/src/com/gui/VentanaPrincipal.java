b89a803cc50e3794614ae50f44b331e1de07287a
Yeisson Rojas <TOSHIBA@192.168.1.6>
2015-11-02 20:54:54 -0500

    Creación de lista de procesos

--- Simulacion_proceso/src/com/gui/VentanaPrincipal.java
+++ Simulacion_proceso/src/com/gui/VentanaPrincipal.java
@@ -2,6 +2,8 @@ package com.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,129 +13,164 @@ import javax.swing.JTextField;
 
 import com.logica.Proceso;
 
+/**
+ * Clase que cuenta con los componentes de la interfaz gráfica y que administra el hilo del proceso
+ * 
+ * @author Alfonso Calero, Fabio Rivera, Yeisson Rojas
+ *
+ */
 public class VentanaPrincipal extends JFrame implements Runnable{
 	
-	 private JLabel nombre;
-	 private JLabel tiempoEjecucion;
-	 private JLabel cronometro;
+	private JLabel nombre;
+	private JLabel tiempoEjecucion;
+	private JLabel cronometro;
+	private JLabel nombreProcesoEjecucion;
 	 
-	 private JTextField nombreTexto;
-	 private JTextField tiempoTexto;
+	private JTextField nombreTexto;
+	private JTextField tiempoTexto;
 	 
-	 private JButton iniciar;
-	 private JButton detener;
-	 private JButton reanudar;
+	private JButton agregar;
+	private JButton iniciar;
+	private JButton detener;
+	private JButton reanudar;
 		
-	 private Proceso proceso;
+	private Proceso proceso;
 		
-	 private Thread hilo;
-	 private boolean ejecucion;
-	 private boolean paused = false;
-	 private boolean stopped = false;
+	private Thread hilo;
+	private boolean ejecucion;
+	private boolean paused = false;
     
-	 public VentanaPrincipal(){ 
-		 this.setTitle("Simulación de Procesos");
-		 this.setSize( 350, 270 );
-		 this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		 this.setLayout(null);
-		 this.setLocationRelativeTo(null);
-		
-		 this.proceso=new Proceso();
-
-		 this.nombre=new JLabel("Nombre de Proceso");
-		 this.nombre.setBounds(20, 20, 150, 30);
-		
-		 this.tiempoEjecucion=new JLabel("Tiempo de Ejecución");
-		 this.tiempoEjecucion.setBounds(20, 60, 150, 30);
-		
-		 this.nombreTexto=new JTextField();
-		 this.nombreTexto.setBounds(180, 20, 130, 30);
+	private ArrayList<Proceso> listaProcesos;
 	
-		 this.tiempoTexto=new JTextField();
-		 this.tiempoTexto.setBounds(180, 60, 130, 30);
+	public VentanaPrincipal(){ 
+		this.setTitle("Simulación de Procesos");
+		this.setSize( 350, 300 );
+		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		this.setLayout(null);
+		this.setLocationRelativeTo(null);
 		
-		 this.iniciar = new JButton("Iniciar");
-		 this.iniciar.setBounds(123, 100, 100, 30);
-		 this.iniciar.addActionListener(new ActionListener() {
+		this.proceso=new Proceso();
+		
+		this.listaProcesos=new ArrayList<Proceso>();
+		
+		this.nombre=new JLabel("Nombre de Proceso");
+		this.nombre.setBounds(20, 20, 150, 30);
+		
+		this.tiempoEjecucion=new JLabel("Tiempo de Ejecución");
+		this.tiempoEjecucion.setBounds(20, 60, 150, 30);
+		
+		this.nombreTexto=new JTextField();
+		this.nombreTexto.setBounds(180, 20, 130, 30);
+	
+		this.tiempoTexto=new JTextField();
+		this.tiempoTexto.setBounds(180, 60, 130, 30);
+		
+		this.agregar=new JButton("Agregar Proceso");
+		this.agregar.setBounds(98, 100, 150, 30);
+		this.agregar.addActionListener(new ActionListener() {
 			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				listaProcesos.add(new Proceso(nombreTexto.getText(),Integer.parseInt(tiempoTexto.getText())));
+				proceso();
+			}
+		});
+		
+		this.iniciar = new JButton("Iniciar");
+		this.iniciar.setBounds(123, 135, 100, 30);
+		this.iniciar.addActionListener(new ActionListener() {
+		
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				iniciarCronometro();
 			}
 		});
 		 
-		 this.cronometro = new JLabel("Tiempo restante de ejecución: 0 segundos");
-		 this.cronometro.setHorizontalAlignment(JLabel.CENTER);
-		 this.cronometro.setBounds(0, 130, 330, 50);
+		this.nombreProcesoEjecucion = new JLabel("Proceso en ejecución:");
+		this.nombreProcesoEjecucion.setHorizontalAlignment(JLabel.CENTER);
+		this.nombreProcesoEjecucion.setBounds(0, 165, 330, 30);
         
-		 this.detener = new JButton("Detener");
-		 this.detener.setBounds(40, 180, 100, 30);
-		 this.detener.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		 });
-		 
-		 this.reanudar = new JButton("Reanudar");
-		 this.reanudar.setBounds(195, 180, 100, 30);
-		 this.reanudar.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		 });
-		 
-		 this.add(nombre);
-		 this.add(nombreTexto);
-		 this.add(tiempoEjecucion);
-		 this.add(tiempoTexto);
-		 this.add(cronometro);
-		 this.add(iniciar);  
-		 this.add(detener);
-		 this.add(reanudar);
-	 }
-  
-	 public void iniciarCronometro() {
-		 ejecucion = true;
-		 hilo = new Thread(this);
-		 hilo.start();
-	 }
-	  
-	 public synchronized void pause() {
-		 paused = true;
-	 }
-
-	 public synchronized void resume() {
-		 paused = false;
-		 notify();
-	 }
-
-	 public synchronized void stop() {
-		 stopped = true;
-		 notify();
-	 }
-	 
-	 public void run(){
-		 proceso=new Proceso(nombreTexto.getText(),Integer.parseInt(tiempoTexto.getText()));
-		 String textoCronometro="";
-		 
-		 try{
-			 while(ejecucion){
-				 Thread.sleep(1000);
-				 proceso.setTiempoEjecucion(proceso.getTiempoEjecucion()-1);
-                 
-				 if(proceso.getTiempoEjecucion() == 0){
-					 JOptionPane.showMessageDialog(null, "Proceso '"+proceso.getNombre()+"' finalizado");
-				 }
-				 else textoCronometro = String.valueOf(proceso.getTiempoEjecucion());
-                 
-				 cronometro.setText("Tiempo restante de ejecución: "+textoCronometro+" segundos");                
-            }
+		this.cronometro = new JLabel("Tiempo restante de ejecución: 0 segundos");
+		this.cronometro.setHorizontalAlignment(JLabel.CENTER);
+		this.cronometro.setBounds(0, 195, 330, 30);
 		
-        }catch(Exception e){}
-        cronometro.setText("Tiempo restante de ejecución: 0 segundos" );
-    }
+		this.detener = new JButton("Detener");
+		this.detener.setBounds(40, 225, 100, 30);
+		this.detener.addActionListener(new ActionListener() {
+		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pause();
+			}
+		});
+		 
+		this.reanudar = new JButton("Reanudar");
+		this.reanudar.setBounds(195, 225, 100, 30);
+		this.reanudar.addActionListener(new ActionListener() {
+		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				resume();
+			}
+		});
+		 
+		this.add(nombre);
+		this.add(nombreTexto);
+		this.add(tiempoEjecucion);
+		this.add(tiempoTexto);
+		this.add(nombreProcesoEjecucion);
+		this.add(cronometro);
+		this.add(iniciar);  
+		this.add(detener);
+		this.add(reanudar);
+		this.add(agregar);
+	}
+  
+	public void iniciarCronometro() {
+		ejecucion = true;
+		hilo = new Thread(this);
+		hilo.start();
+	}
+	  
+	public synchronized void pause() {
+		paused = true;
+	}
+
+	public synchronized void resume() {
+		paused = false;
+		notify();
+	}
+	 
+	public void proceso(){
+		
+	}
+	public void run(){
+		String textoCronometro="";
+		String textoProceso="";
+		
+		while (ejecucion) {
+			for (int i = 0; i < listaProcesos.size(); i++) {
+				proceso=listaProcesos.get(i);
+				try {
+					synchronized (this) {
+						textoProceso = proceso.getNombre();
+						nombreProcesoEjecucion.setText("Proceso en ejecución: "+textoProceso);
+						textoCronometro = String.valueOf(proceso.getTiempoEjecucion());
+						cronometro.setText("Tiempo restante de ejecución: "+textoCronometro+" segundos");	 
+						if (paused) {
+							wait();
+						}
+					}
+					Thread.sleep(100);
+					proceso.setTiempoEjecucion(proceso.getTiempoEjecucion()-1);
+					if(proceso.getTiempoEjecucion()<0){
+						paused=true;
+						JOptionPane.showMessageDialog(null,"Proceso '"+proceso.getNombre()+"' finalizado");
+					}
+				} 
+				catch(Exception e){}
+					cronometro.setText("Tiempo restante de ejecución: 0 segundos" );
+		 	}
+		}
+	}
 }
--- Simulacion_proceso/src/com/logica/Proceso.java
+++ Simulacion_proceso/src/com/logica/Proceso.java
@@ -1,31 +1,73 @@
 package com.logica;
 
+import java.util.ArrayList;
+
+/**
+ * Clase que guarda la información del proceso que se ejecuta
+ * @author Alfonso Calero, Fabio Rivera, Yeisson Rojas
+ *
+ */
 public class Proceso {
+	
+	/**
+	 * Atributo de tipo String del nombre del proceso
+	 */
 	private String nombre;
+	
+	/**
+	 * Atributo de tipo Integer del tiempo que se va a ejecutar el proceso
+	 */
 	private int tiempoEjecucion;
 	
+	/**
+	 * Constructor de la clase Proceso. Recibe por parámetro el nombre y el tiempo de ejecución
+	 * @param nombre
+	 * @param tiempoEjecucion
+	 */
 	public Proceso(String nombre, int tiempoEjecucion) {
 		this.nombre=nombre;
 		this.tiempoEjecucion=tiempoEjecucion;
 	}
 	
+	/**
+	 * Constructor por defecto de la clase Proceso
+	 */
 	public Proceso(){
 		this.nombre=null;
 		this.tiempoEjecucion=0;
 	}
 
+	public String toString(){
+		return "Nombre: "+nombre+" Tiempo de Ejecución: "+tiempoEjecucion;
+	}
+	/**
+	 * Método get del nombre del proceso
+	 * @return nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Método set del nombre del proceso
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Método get del tiempo de ejecución del proceso
+	 * @return nombre
+	 */
 	public int getTiempoEjecucion() {
 		return tiempoEjecucion;
 	}
 
+	/**
+	 * Método set del tiempo de ejecución del proceso
+	 * @param tiempoEjecucion
+	 */
 	public void setTiempoEjecucion(int tiempoEjecucion) {
 		this.tiempoEjecucion = tiempoEjecucion;
 	}
